c50863ff92c79ad99851fee40415aa881213da79
Ryan W <1643782914@qq.com>
2024-10-13 22:12:00 +1100

    PostgreSQL Configuration

--- src/Services/Catalog/CatalogAPI/Models/Products/CreateProduct/CreateproductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Models/Products/CreateProduct/CreateproductHandler.cs
@@ -1,6 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-
-namespace CatalogAPI.Models.Products.CreateProduct
+﻿namespace CatalogAPI.Models.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
             :ICommand<CreateProductResult>;
@@ -8,6 +6,13 @@ namespace CatalogAPI.Models.Products.CreateProduct
     internal class CreateproductCommandHandler 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        private readonly IDocumentSession _session;
+
+        // Constructor to inject the session
+        public CreateproductCommandHandler(IDocumentSession session)
+        {
+            _session = session;
+        }
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             //Create Product entity
@@ -20,12 +25,12 @@ namespace CatalogAPI.Models.Products.CreateProduct
                 Price = command.Price,
             };
 
-            //Save to Database ---TODO
+            //Save to Database
+            _session.Store(product);
+            await _session.SaveChangesAsync(cancellationToken);
 
             //Return the result
-            return new CreateProductResult(Guid.NewGuid());
-
-            throw new NotImplementedException();
+            return new CreateProductResult(product.Id);
         }
     }
 }
