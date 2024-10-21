63c1d03359493781beba03e56296152253398ae0
Ryan W <1643782914@qq.com>
2024-10-21 21:22:51 +1100

    Implement UpdateProductById handler and endpoint

---
+++ src/Services/Catalog/CatalogAPI/Models/Products/UpdateProductById/UpdateProductByIdEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using CatalogAPI.Models.Products.GetProductById;
+
+namespace CatalogAPI.Models.Products.UpdateProductById
+{
+    public record UpdateProductByIdRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record UpdateProductByIdResponse(bool IsSuccess);
+    public class UpdateProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductByIdRequest request, ISender sender ) => 
+            {
+                var command = request.Adapt<UpdateProductByIdCommand>();
+                var result = await sender.Send(request);
+                var response = result.Adapt<UpdateProductByIdResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("UpdateProductById")
+                .Produces<UpdateProductByIdResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Update Product by Id")
+                .WithDescription("Update Product By Id"); ;
+
+        }
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Models/Products/UpdateProductById/UpdateProductByIdHandler.cs
@@ -0,0 +1,40 @@
+﻿
+namespace CatalogAPI.Models.Products.UpdateProductById
+{
+    public record UpdateProductByIdCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductByIdResult>;
+    public record UpdateProductByIdResult(bool IsSuccess);
+
+    internal class UpdateProductByIdCommandHandler 
+        : ICommandHandler<UpdateProductByIdCommand, UpdateProductByIdResult>
+    {
+        private readonly IDocumentSession _session;
+        private readonly ILogger _logger;
+        public UpdateProductByIdCommandHandler(IDocumentSession session, ILogger logger)
+        {
+            _session = session;
+            _logger = logger;
+        }
+        public async Task<UpdateProductByIdResult> Handle(UpdateProductByIdCommand command, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("UpdateProductByIdCommandHandler.Handle called with {@Command}", command);
+
+            var product = await _session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product == null) 
+            {
+                throw new ProductNotFoundException();
+            }
+
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+
+            _session.Update(product);
+            await _session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductByIdResult(true);
+        }
+    }
+}
