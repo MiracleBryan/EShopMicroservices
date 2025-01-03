9291961c494bf4c4b7eb87781edd229104fb4d3d
Ryan W <1643782914@qq.com>
2025-01-03 23:18:53 +1100

    Built GetBasketByUserName API

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,14 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //TODO GET DATABASE
+            //var basket = await _repository.GetBasket(request.UserName);
+            return new GetBasketResult(new ShoppingCart("ryan's"));
+        }
     }
 }
