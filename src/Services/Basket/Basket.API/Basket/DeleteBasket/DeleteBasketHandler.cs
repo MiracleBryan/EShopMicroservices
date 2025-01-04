daa3b67808538db447fc5de6ad8b76c8a9bf87b0
Ryan W <1643782914@qq.com>
2025-01-04 15:41:25 +1100

    Built DeleteBasket API

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,23 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+    public class DeleteBasketCommandValidatior : AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidatior()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+        {
+            //TODO - DELETE BASKET
+            //session.Delete<Product>(command.Id);
+
+            return new DeleteBasketResult(true);
+        }
     }
 }
